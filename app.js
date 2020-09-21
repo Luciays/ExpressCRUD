5e295301a0e676db757cb34e716ac5805eab0990
Yusun <ys.choe@promptech.co.kr>
2020-09-21 22:00:16 +0900

    session and router setting: app.js / user.js

--- app.js
+++ app.js
@@ -7,7 +7,45 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var crypto = require('crypto');
+var mysql = require('mysql');
+var session = require("express-session");
+var MySQLStore = require('express-mysql-session')(session);
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+
+var userRouter = require('./routes/user');
+
+var connection = mysql.createConnection({
+  host: process.env.SESSIONSDB_HOST,
+  port: process.env.SESSIONSDB_PORT,
+  user: process.env.SESSIONSDB_USER,
+  password: process.env.SESSIONSDB_PASS,
+  database: process.env.SESSIONSDB_DB
+});
+
+var sessionStore = new MySQLStore({
+  checkExpirationInterval: parseInt(process.env.SESSIONSDB_CHECK_EXP_INTERVAL, 10),
+  expiration: parseInt(process.env.SESSIONSDB_EXPIRATION, 10)
+}, connection);
+
 var app = express();
+
+/* Create a cookie that expires in 1 day */
+var expireDate = new Date();
+expireDate.setDate(expireDate.getDate() + 1);
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSIONSDB_SECRET,
+  store: sessionStore,
+  cookie: { expires: expireDate }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/user', userRouter);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
